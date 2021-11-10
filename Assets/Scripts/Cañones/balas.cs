be4d776618286a2aae779714e4e91d7d1c31a0dc
JuanmaPensado <90263386+JuanmaPensado@users.noreply.github.com>
2021-11-10 12:39:40 +0100

    cañones arreglados

--- Assets/Scripts/Cañones/balas.cs
+++ Assets/Scripts/Cañones/balas.cs
@@ -6,17 +6,28 @@ public class balas : MonoBehaviour
 {
 
     public float launchVelocity = 700f;
+    public float flytime = 3f;
     Rigidbody2D rb;
+    CañonesEstaticos ultimo_cañon;
+    public GameObject cañonInicial;
 
     void Start(){
         rb = GetComponent<Rigidbody2D> ();
         rb.AddRelativeForce(new Vector3 (launchVelocity,0 ,0));
+        cañonInicial = GameObject.Find("cañonEstaticoInicial/launch_origin");
+        ultimo_cañon = cañonInicial.GetComponent<CañonesEstaticos>();
+        Invoke("DestroyBullet",flytime);
     }
 
     void OnTriggerEnter2D(Collider2D otro){
         if (otro.gameObject.tag == "cañon" ){
-            Debug.Log(otro);
-            Destroy(gameObject);
+            ultimo_cañon = otro.GetComponent<CañonesEstaticos>();
+            DestroyBullet();
         }
+    }
+
+    void DestroyBullet(){
+        ultimo_cañon.cargado = true;
+        Destroy(gameObject);
     }
 }
